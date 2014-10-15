37b01ee8a86cc5ca34ac33d61b2e50dd788fadcd
Damian Manda <damian@dmanda.com>
2014-10-15 18:34:48 -0400

    Arduino Code simulating a radio control override

---
+++ Arduino/RadioOverride/RadioOverride.ino
@@ -0,0 +1,39 @@
+/* 
+Servo position sweeps unless overridden by a potentiometer
+
+ modified on 15 Oct 2014
+ by Damian Manda
+ */
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+ 
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+int pos = 1;  //servo position
+int dir = 1;
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+void loop() 
+{
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  if (val < 300) {                     //
+    if (pos < 180 && pos > 0) {
+      pos += dir;
+    } else {
+      pos -= dir;
+      dir = dir * -1;
+    }
+    myservo.write(pos);
+    delay(15);
+  } else {
+    val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180) 
+    myservo.write(val);                  // sets the servo position according to the scaled value 
+    delay(15);   // waits for the servo to get there 
+  }
+} 
