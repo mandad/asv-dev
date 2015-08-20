37100913aa8a9d4986c2a7eb05aca871c6df1572
Damian Manda <damian9@gmail.com>
2015-08-20 15:41:44 -0400

    Fixed error

--- utilities/ZBoat_testing/pwm_test.py
+++ utilities/ZBoat_testing/pwm_test.py
@@ -1,10 +1,12 @@
 import sys
+import time
 import serial
 
 class ZBoatTester(object):
     def __init__(self, serial_port='/dev/ttyUSB0', baudrate=115200):
         self.serial_port = serial_port
         self.baudrate = baudrate
+        self.port_open = False
         self.ser = None
 
     def open_port(self, timeout=10):
@@ -21,9 +23,11 @@ class ZBoatTester(object):
         self.set.close()
 
     def set_autonomy_mode(self):
+        print('Setting Autonomous Control Mode\n')
         self.ser.write('!SetAutonomousControl\r\n')
 
     def set_manual_mode(self):
+        print('Setting Manual Control Mode\n')
         self.ser.write('!SetManualControl\r\n')
 
     def write_pwm_values(self, left_thrust, right_thrust, rudder):
@@ -33,27 +37,37 @@ class ZBoatTester(object):
         right_thrust = clamp(right_thrust, 1.0, 2.0)
         rudder = clamp(rudder, 1.0, 2.0)
 
+        print('Writing PWM values r_thrust=%4.3f, l_thrust=%4.3f, rudder=%4.3f')
         self.ser.write('!pwm, *, %4.3f, %4.3f, %4.3f, *, *\r\n'.format( \
             left_thrust, right_thrust, rudder))
 
+    def write_loop(self, left_thrust, right_thrust, rudder):
+        try:
+            while True:
+                write_pwm_values(left_thrust, right_thrust, rudder)
+                time.sleep(0.5)
+        except KeyboardInterrupt:
+            print('Exiting write loop')
+            return
+
 
 def main(argv):
-    if len(argv == 4):
-        thisTester = ZBoatTester(argv[3])
+    if len(argv) == 4:
+        this_tester = ZBoatTester(argv[3])
     else:
-        thisTester = ZBoatTester()
-    thisTester.open_port()
-    thisTester.set_autonomy_mode()
+        this_tester = ZBoatTester()
+    this_tester.open_port()
+    this_tester.set_autonomy_mode()
 
     if len(argv) == 2:
         # args = thrust, rudder
-        thisTester.write_pwm_values(argv[0], argv[0], argv[1])
+        this_tester.write_pwm_values(argv[0], argv[0], argv[1])
     elif len(argv) >= 3:
         # args = left_thrust, right_thrust, rudder
-        thisTester.write_pwm_values(argv[0], argv[1], argv[2])
+        this_tester.write_pwm_values(argv[0], argv[1], argv[2])
 
-    thisTester.set_manual_mode()
-    thisTester.close_port()
+    this_tester.set_manual_mode()
+    this_tester.close_port()
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
