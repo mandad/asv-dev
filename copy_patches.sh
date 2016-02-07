a30348b4df4289028a44fbecff03c14455ddcd84
Damian Manda <damian9@gmail.com>
2016-02-07 13:21:43 -0500

    Added patch copying script

---
+++ copy_patches.sh
@@ -0,0 +1,3 @@
+# Copy the patches to the main MOOS-IvP tree
+cp ../essential-moos/Essentials/pLogger/MOOSLogger.* ../moos-ivp/MOOS/MOOSEssentials/Essentials/pLogger/
+cp ../core-moos/Core/libMOOS/Comms/ThreadedCommServer.cpp ../moos-ivp/MOOS/MOOSCore/Core/libMOOS/Comms/
