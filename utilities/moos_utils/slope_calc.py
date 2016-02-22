3cfe3aadb2cb7258327ebab61eb16e854546a908
Damian Manda <damian9@gmail.com>
2016-02-22 00:51:05 -0500

    Fixed initial speed adjust

--- utilities/moos_utils/slope_calc.py
+++ utilities/moos_utils/slope_calc.py
@@ -2,11 +2,12 @@ import slog_graph
 import numpy as np
 import matplotlib.pyplot as plt
 
-filter_len = 30;
+time_len = 3 #seconds
+filter_len = 10 * time_len
 
 slog_rough = slog_graph.SLogGraph("MOOSLog_14_1_2016_____20_48_37.slog")
 speeds_rough = slog_rough.get_col_data(16)
-slopes_rough = [(speeds_rough[x] - speeds_rough[x-filter_len])/(filter_len/3) \
+slopes_rough = [(speeds_rough[x] - speeds_rough[x-filter_len])/(filter_len/time_len) \
     for x in np.arange(speeds_rough.size-filter_len)+filter_len]
 slopes_rough=np.append(np.zeros(filter_len),slopes_rough)
 
